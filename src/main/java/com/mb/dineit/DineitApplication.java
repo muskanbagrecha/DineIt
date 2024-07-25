d2c55a160ebd23d5ed90fb03b79950ef29225138
muskanbagrecha <muskanbagrecha04@gmail.com>
2024-07-25 23:08:56 +0530

    git commit -m "initial commit"

---
+++ src/main/java/com/mb/dineit/DineitApplication.java
@@ -0,0 +1,13 @@
+package com.mb.dineit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DineitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DineitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mb/dineit/DineitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mb.dineit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DineitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
